f5e2e470904cca941b47ce53fb64ca68cac34ca4
jingjingtapa <150122081+jingjingtapa@users.noreply.github.com>
2023-11-07 18:40:26 +0900

    파이썬에서 시리얼모니터 관찰하기(아두이노코드)

---
+++ serial/serial.ino
@@ -0,0 +1,12 @@
+int output_data = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(output_data++);
+  delay(500); // delay 500ms
+}
