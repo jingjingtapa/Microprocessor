1fadf7bf6a41c275d0e09d14da9aa66285bab001
jingjingtapa <jingjingtapa@naver.com>
2023-11-18 12:57:12 +0900

    Integrate Airquality Sensor

--- EAR/EAR.py
+++ EAR/EAR.py
@@ -9,9 +9,10 @@ import time
 import dlib
 import cv2
 import math
+import serial
 
 
-
+# EAR 함수 정의
 def eye_aspect_ratio(eye):
     A = dist.euclidean(eye [1], eye[5])
     B = dist.euclidean(eye [2], eye[4])
@@ -19,13 +20,13 @@ def eye_aspect_ratio(eye):
     ear = (A+B) / (2.0 * C)
     return ear
 
-
+# 눈 감음 기준값 설정
 EYE_AR_THRESH = 0.2
-EYE_AR_CONSEC_FRAMES = 2
+EYE_AR_CONSEC_FRAMES = 30
 COUNTER = 0
 TOTAL = 0
 
-
+# 안면 인식 기본 설정
 print("[INFO] loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
@@ -34,59 +35,61 @@ predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
+# 카메라 불러오기
 print("[INFO] starting video stream thread...")
-vs = VideoStream(src=0).start()
+vs = VideoStream(src=1).start()
 time.sleep(1.0)
 
-start = time.time()
+ser = serial.Serial('COM5', 9600, timeout=1)
+time.sleep(2)
 
 while True:
-    frame = vs.read()
-    frame = imutils.resize(frame, width=450)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    rects = detector(gray, 0)
-    for rect in rects:
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
-        leftEye = shape[lStart:lEnd]
-        rightEye = shape[rStart:rEnd]
-        leftEAR = eye_aspect_ratio(leftEye)
-        rightEAR = eye_aspect_ratio(rightEye)
-        ear = (leftEAR + rightEAR) / 2.0
-        leftEyeHull = cv2.convexHull(leftEye)
-        rightEyeHull = cv2.convexHull(rightEye)
-        cv2.drawContours (frame, [leftEyeHull], -1, (0, 255, 0), 1) 
-        cv2.drawContours (frame, [rightEyeHull], -1, (0, 255, 0), 1)
-        print(ear)
-
-        now = time.time()
-        TIME = now - start
-        if ear < EYE_AR_THRESH:
-            COUNTER += 1
-        else:
-            if COUNTER >= EYE_AR_CONSEC_FRAMES:
-                TOTAL += 1
-            COUNTER = 0
-
-        cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    get_data = ser.readline()
+    if get_data:
+        get_data = get_data.decode()
+        num = int(get_data)
+        print(num)
+        if num:
+            frame = vs.read()
+            frame = imutils.resize(frame, width=450)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            rects = detector(gray, 0)
+            for rect in rects:
+                shape = predictor(gray, rect)
+                shape = face_utils.shape_to_np(shape)
+                leftEye = shape[lStart:lEnd]
+                rightEye = shape[rStart:rEnd]
+                leftEAR = eye_aspect_ratio(leftEye)
+                rightEAR = eye_aspect_ratio(rightEye)
+                ear = (leftEAR + rightEAR) / 2.0
+                leftEyeHull = cv2.convexHull(leftEye)
+                rightEyeHull = cv2.convexHull(rightEye)
+                cv2.drawContours (frame, [leftEyeHull], -1, (0, 255, 0), 1) 
+                cv2.drawContours (frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
 
-        if TIME > 0:
-            cv2.putText(frame, "Time: {:.2f}".format(TIME), (300, 100),
+                if ear < EYE_AR_THRESH:
+                    COUNTER += 1
+                    if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                        TOTAL += 10
+                        COUNTER=0
+                else:
+                    COUNTER = 0
+
+                cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        if TIME > 10 and TOTAL < 5:
-            cv2.putText(frame, "Warning", (10, 100),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+
+
                         
 
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
+            cv2.imshow("Frame", frame)
+            key = cv2.waitKey(1) & 0xFF
 
-    if key == ord("q"):
-        break
+            if key == ord("q"):
+                break
 
 cv2.destroyAllWindows()
 vs.stop()
--- serial/DistanceSensor/VL53L0X/VL53L0X.ino
+++ serial/DistanceSensor/VL53L0X/VL53L0X.ino
@@ -1,6 +1,20 @@
 #include "Adafruit_VL53L0X.h"
+#include <DFRobot_ENS160.h>
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+#define I2C_COMMUNICATION  //I2C communication. Comment out this line of code if you want to use SPI communication.
+#ifdef  I2C_COMMUNICATION  //For Fermion version, the default I2C address is 0x53, connect SDO pin to GND and I2C address will be 0x52
+
+  DFRobot_ENS160_I2C ENS160(&Wire, /*I2CAddr*/ 0x53);
+#else
+  /**
+   * Set up digital pin according to the on-board pin connected with SPI chip-select pin
+   * csPin Available Pins. For example: ESP32&ESP8266(D3), m0(6)
+   */
+  uint8_t csPin = D3;
+  DFRobot_ENS160_SPI ENS160(&SPI, csPin);
+#endif
 
 ////////////// 센서 핀 위치 설정 //////////////////
 
@@ -14,7 +28,7 @@ int forcepin = A1;
 int BACKLIGHT = 8;  ///// 후미등 input
 int speakerPin = 9; ///// 부저스피커 input
 int tones[]={261,277,311}; // 연주할 톤의 진동수.
-int delays = 30;
+int delays = 500;
 ////////////// 졸음 판단 기준 변수들 /////////////////
 
 int HEAD = 130;  
@@ -33,13 +47,18 @@ void setup() {
     delay(1);
   }
   
+  while( NO_ERR != ENS160.begin() ){
+    Serial.println("Air quality sensor failed.");
+    delay(delays);
+  }
+
   // Serial.println("Adafruit VL53L0X ");
   if (!lox.begin()) {
-     Serial.println(F("Failed to boot VL53L0X"));
+     Serial.println(F("Distance Sensor failed."));
      while(1);
    }
-   // power 
-  // Serial.println(F("Start checking Distance...")); 
+  ENS160.setPWRMode(ENS160_STANDARD_MODE); 
+  ENS160.setTempAndHum(/*temperature=*/25.0, /*humidity=*/50.0);
 }
 
 
@@ -50,13 +69,16 @@ void loop() {
   lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
   
   int force = analogRead(forcepin); // 압전센서의 측정값
-  
+  uint16_t ECO2 = ENS160.getECO2(); // CO2 농도 측정값
+  Serial.print("Carbon dioxide equivalent concentration : ");Serial.print(ECO2);Serial.println(" ppm");
+
   if (measure.RangeStatus != 4) {  // phase failures have incorrect data
     
     if (measure.RangeMilliMeter >= HEAD & force <= GRIP ) {
 
       Serial.print("distance : "); Serial.print(measure.RangeMilliMeter); Serial.print(", force : "); Serial.print(force); Serial.println(" - WAKE UP");
-      digitalWrite(BACKLIGHT,HIGH);
+
+      digitalWrite(BACKLIGHT,HIGH);  // 후미등 껐다 켜기
       delay(delays);
       digitalWrite(BACKLIGHT,LOW);
       delay(delays);
